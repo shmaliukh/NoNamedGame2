3087c843f627d8794ed83726bfe29cca2f1fce05
vshmaliukh <zskat02@gmail.com>
2023-04-21 15:06:26 +0300

    Created base javafx module

---
+++ game-client/src/main/java/xyz/vshmaliukh/gameclient/ClientApplication.java
@@ -0,0 +1,21 @@
+package xyz.vshmaliukh.gameclient;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ClientApplication extends Application {
+
+    public static Stage mainStage;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ game-client/src/main/java/xyz/vshmaliukh/gameclient/Launcher.java
@@ -0,0 +1,9 @@
+package xyz.vshmaliukh.gameclient;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        ClientApplication.main(args);
+    }
+
+}
