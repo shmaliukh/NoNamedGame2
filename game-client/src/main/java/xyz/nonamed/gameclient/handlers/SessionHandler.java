a652a609c7fb34d92fcbf7b58ef471f21331606f
vshmaliukh <zskat02@gmail.com>
2023-04-21 15:54:51 +0300

    Implemented handlers (to call feign)

---
+++ game-client/src/main/java/xyz/nonamed/gameclient/handlers/SessionHandler.java
@@ -0,0 +1,35 @@
+package xyz.nonamed.gameclient.handlers;
+
+import feign.Feign;
+import feign.gson.GsonDecoder;
+import feign.gson.GsonEncoder;
+import xyz.nonamed.dto.Session;
+import xyz.nonamed.gameclient.client_protocols.SessionAPI;
+
+import java.util.List;
+
+import static xyz.nonamed.Constants.BASE_URL;
+
+public class SessionHandler implements SessionAPI {
+
+    public SessionAPI sessionAPI = Feign.builder()
+            .encoder(new GsonEncoder())
+            .decoder(new GsonDecoder())
+            .target(SessionAPI.class, BASE_URL);
+
+    @Override
+    public Session postConnectUserToSession(String userName, String sessionCode) {
+        return sessionAPI.postConnectUserToSession(userName, sessionCode);
+    }
+
+    @Override
+    public List<Session> getAllSessions() {
+        return sessionAPI.getAllSessions();
+    }
+
+    @Override
+    public boolean postRunSession(String userName, String sessionCode) {
+        return sessionAPI.postRunSession(userName, sessionCode);
+    }
+
+}
---
+++ game-client/src/main/java/xyz/nonamed/gameclient/handlers/UserHandler.java
@@ -0,0 +1,30 @@
+package xyz.nonamed.gameclient.handlers;
+
+import feign.Feign;
+import feign.gson.GsonDecoder;
+import feign.gson.GsonEncoder;
+import xyz.nonamed.dto.User;
+import xyz.nonamed.gameclient.client_protocols.UserAPI;
+
+import java.util.List;
+
+import static xyz.nonamed.Constants.BASE_URL;
+
+public class UserHandler implements UserAPI {
+
+    public UserAPI userAPI = Feign.builder()
+            .decoder(new GsonDecoder())
+            .encoder(new GsonEncoder())
+            .target(UserAPI.class, BASE_URL);
+
+    @Override
+    public User postRegisterUser(String userName) {
+        return userAPI.postRegisterUser(userName);
+    }
+
+    @Override
+    public List<User> getUsersBySessionCode(String userName, String sessionCode) {
+        return userAPI.getUsersBySessionCode(userName, sessionCode);
+    }
+
+}
