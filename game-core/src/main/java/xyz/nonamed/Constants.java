36a5fef14fa639becff18dc18cd4fe79fe4334a1
vshmaliukh <zskat02@gmail.com>
2023-04-21 15:09:19 +0300

    Created base config for urls

---
+++ game-core/src/main/java/xyz/nonamed/Constants.java
@@ -0,0 +1,12 @@
+package xyz.nonamed;
+
+public final class Constants {
+
+    private Constants(){}
+
+    public static final String AWS_URL = "http://"; // TODO
+    public static final String LOCALHOST_URL = "http://localhost:8082/";
+    public static final String BASE_URL = LOCALHOST_URL;
+
+
+}
