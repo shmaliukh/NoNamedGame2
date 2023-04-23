a4ddc801bfd029aa4bb317d1146226263d12b542
vshmaliukh <zskat02@gmail.com>
2023-04-23 14:18:18 +0300

    Added server switch

--- game-core/src/main/java/xyz/nonamed/Constants.java
+++ game-core/src/main/java/xyz/nonamed/Constants.java
@@ -6,7 +6,7 @@ public final class Constants {
 
     public static final String AWS_URL = "http://testgame1.eu-north-1.elasticbeanstalk.com/"; // TODO
     public static final String LOCALHOST_URL = "http://localhost:8082/";
-    public static final String BASE_URL = LOCALHOST_URL;
+    public static String BASE_URL = LOCALHOST_URL;
 
     public static final String MOVE_UP = "move_up";
     public static final String MOVE_DOWN = "move_down";
